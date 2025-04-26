c053467188c4f11ab09ce159111428e5fe8035ae
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2025-04-26 17:34:52 +0700

    chore(2021): day 06 - part 1

---
+++ 2021/day-06/part-1/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"cdf144/aoc2021/utils"
+	"path/filepath"
+	"strings"
+)
+
+func updateLanternfishes(lanternfishes *[]int) {
+	for i, fish := range *lanternfishes {
+		if fish == 0 {
+			(*lanternfishes)[i] = 6
+			*lanternfishes = append(*lanternfishes, 8)
+		} else {
+			(*lanternfishes)[i]--
+		}
+	}
+}
+
+func main() {
+	input, printAnswer := utils.InitRaw(filepath.Join("..", "input.example.txt"))
+	inputSplit := strings.Split(strings.TrimRight(input, "\r\n"), ",")
+
+	lanternfishes := make([]int, len(inputSplit))
+	for i, s := range inputSplit {
+		lanternfishes[i] = utils.Atoi(s)
+	}
+
+	for range 80 {
+		updateLanternfishes(&lanternfishes)
+	}
+
+	printAnswer(len(lanternfishes))
+}
