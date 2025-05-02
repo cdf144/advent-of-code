1370e5efbc5601e9fdfdd9f37907018864e76a29
Trần Đình Phú <56244176+cdf144@users.noreply.github.com>
2025-05-02 11:11:55 +0700

    chore(2021): day 08 - part 1

---
+++ 2021/day-08/part-1/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"cdf144/aoc2021/utils"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	lines, printAnswer := utils.Init(filepath.Join("..", "input.example.txt"))
+
+	var answer int
+
+	for _, line := range lines {
+		outputDigits := strings.Fields(
+			strings.Split(line, " | ")[1],
+		)
+		for _, digit := range outputDigits {
+			switch len(digit) {
+			case 2, 3, 4, 7:
+				answer++
+			}
+		}
+	}
+
+	printAnswer(answer)
+}
